2e35f104cfa494c0d1138d51764f6521169c188c
MmD <mmdj27634@gmail.com>
2023-08-14 15:33:19 -0700

    add : icons and icon component for using them with backup icon

---
+++ components/icons.tsx
@@ -0,0 +1,26 @@
+import { IconType } from "react-icons";
+import {
+  RiCheckboxBlankCircleLine,
+  RiDiscordFill,
+  RiEyeCloseLine,
+  RiEyeLine,
+  RiFacebookFill,
+  RiFunctionFill,
+  RiGoogleFill,
+  RiLoader5Line,
+  RiUser2Line,
+} from "react-icons/ri";
+
+export const Icons: {
+  [name: string]: IconType;
+} = {
+  User: RiUser2Line,
+  Spinner: RiLoader5Line,
+  Circle: RiCheckboxBlankCircleLine,
+  Discord: RiDiscordFill,
+  Google: RiGoogleFill,
+  Facebook: RiFacebookFill,
+  Logo: RiFunctionFill,
+  Eye: RiEyeLine,
+  EyeClose: RiEyeCloseLine,
+};
