cb8cd8da01bf563a3156d17c5527ab6de3e709d4
MmD <mmdj27634@gmail.com>
2023-08-16 09:30:46 -0700

    add : description placeholder to config

---
+++ config/index.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  name: "Yellow Page",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dignissimos repellat eveniet magnam facilis",
+};
