bf115638b44635ba56b49e92001dfa0a8ad25883
MmD <mmdj27634@gmail.com>
2023-08-18 02:27:53 -0700

    feat(auth) : add custom fetch with a backup plan for expired tokens

---
+++ hooks/use-custom-fetch.ts
@@ -0,0 +1,99 @@
+import { BACKEND_URL } from "@/config/env";
+import { useEffect, useState, useCallback } from "react";
+
+type State = string | undefined;
+
+type CustomFetch = (
+  input: RequestInfo | URL,
+  init?: RequestInit | undefined
+) => Promise<Response>;
+
+let initialState: State;
+
+let listernes: Array<(state: State) => void> = [];
+
+export const dispatch = (state: State) => {
+  initialState = state;
+  listernes.forEach((setState) => setState(initialState));
+};
+
+const refresh_token = async () => {
+  const res = await fetch(BACKEND_URL + "/api/auth/refresh-token", {
+    method: "POST",
+    credentials: "include",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (res.status === 401) return "";
+
+  const { data } = await res.json();
+
+  if (!data?.accessToken) return;
+
+  dispatch(data.accessToken);
+  return data.accessToken;
+};
+
+export const useCustomFetch = () => {
+  const [state, setState] = useState<State>(initialState);
+
+  useEffect(() => {
+    listernes.push(setState);
+
+    return () => {
+      const idx = listernes.indexOf(setState);
+
+      if (idx >= 0) listernes.splice(idx, 1);
+    };
+  }, [state]);
+
+  const custom_fetch = useCallback(
+    async (input: RequestInfo | URL, init?: RequestInit | undefined) => {
+      const res = await fetch(
+        (BACKEND_URL ?? "") + input,
+        init
+          ? {
+              ...init,
+              headers: init.headers
+                ? {
+                    ...init.headers,
+                    Authorization: `Bearer ${state}`,
+                  }
+                : {
+                    Authorization: `Bearer ${state}`,
+                  },
+            }
+          : undefined
+      );
+
+      if (res.status !== 403) return res;
+
+      const new_token = await refresh_token();
+
+      if (!new_token) return res;
+
+      return await fetch(
+        (BACKEND_URL ?? "") + input,
+        init
+          ? {
+              ...init,
+              headers: init.headers
+                ? {
+                    ...init.headers,
+                    Authorization: `Bearer ${new_token}`,
+                  }
+                : {
+                    Authorization: `Bearer ${new_token}`,
+                  },
+            }
+          : undefined
+      );
+    },
+    [state]
+  );
+
+  return custom_fetch;
+};
