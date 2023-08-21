c460a2b457e5946de3e542e6f17167d225c2be93
MmD <mmdj27634@gmail.com>
2023-08-21 12:10:26 -0700

    style : some improvements to 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,11 +2,13 @@ import Link from "next/link";
 
 const NotFound = () => (
   <main className="grid place-items-center w-full h-full bg-background ">
-    <div className="flex flex-col items-center justify-center">
-      <h1 className="text-foreground text-[60px] font-bold">404</h1>
-      <p className="text-foreground/75">
+    <div className="flex flex-col gap-1 items-center justify-center bg-foreground p-12 rounded-lg">
+      <h1 className="text-background text-[60px] leading-none font-bold">
+        404
+      </h1>
+      <p className="text-background/75">
         Are you lost? Lets take you{" "}
-        <Link className="text-foreground underline" href={"/"}>
+        <Link className="text-background underline" href={"/"}>
           Home
         </Link>
         .
