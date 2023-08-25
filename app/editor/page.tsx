bd0fcdf1b340d9f458d0a50fb099a3e25c1d9da0
MmD <mmdj27634@gmail.com>
2023-08-25 08:19:06 -0700

    feat : use editor-view in editor page

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -1,14 +1,9 @@
 import Editor from "@/components/editor";
+import EditorView from "@/components/editor-view";
 import { NextPage } from "next";
 
 const Page: NextPage = ({}) => {
-  return (
-    <div className="w-full h-full">
-      <main className="flex max-w-[1300px] mx-auto px-4 md:px-8 ">
-        <Editor />
-      </main>
-    </div>
-  );
+  return <EditorView />;
 };
 
 export default Page;
