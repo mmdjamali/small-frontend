6b5bf6e049ffc027b18a9fc2a2633c7e1b1feda2
MmD <mmdj27634@gmail.com>
2023-09-05 11:52:13 -0700

    ref : change layout for main

--- app/(main)/tag/[tag]/page.tsx
+++ app/(main)/tag/[tag]/page.tsx
@@ -7,5 +7,13 @@ interface PageProps {
 }
 
 export default async function Page({ params }: PageProps) {
-  return <Feed active={params.tag} />;
+  return (
+    <main className="mx-auto grid min-h-[calc(100vh_-_58px)] max-w-[1300px] grid-cols-1 px-4 md:grid-cols-[1fr_352px] md:gap-8 md:px-8 lg:gap-12">
+      <div className="relative flex h-full w-full max-w-full flex-col pt-2">
+        <Feed active={params.tag} />
+      </div>
+
+      <div className="hidden h-full w-full border-l border-border pl-8 md:flex"></div>
+    </main>
+  );
 }
