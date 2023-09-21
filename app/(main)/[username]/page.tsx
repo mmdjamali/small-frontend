8f787114ca528de31a4238235526d60b7827ede8
MmD <mmdj27634@gmail.com>
2023-09-21 12:13:24 -0700

    add : profile pages

--- app/(main)/[username]/page.tsx
+++ app/(main)/[username]/page.tsx
@@ -1,8 +1,11 @@
 import Icon from "@/components/icon";
-import ProfileActions from "@/components/profile-actions";
+import ProfileActions from "@/components/profile/profile-actions";
+import UserProfileTabs from "@/components/profile/user-profile-tabs";
+import UserStories from "@/components/profile/user-stories";
 import EmptySearchFetch from "@/components/search/empty-search-feed";
 import Button from "@/components/ui/button";
 import { BACKEND_URL } from "@/config/env";
+import { stringifyNumber } from "@/lib/utils";
 import { GetProfileApiResponse } from "@/types/api";
 import { UserType } from "@/types/user";
 import Image from "next/image";
@@ -42,7 +45,7 @@ const Page = async ({ params }: PageProps) => {
 
   return (
     <main className="grid w-full ">
-      <div className="relative mx-auto flex h-full w-full max-w-[800px] flex-col items-start px-4 pt-6 md:px-8 md:pt-16">
+      <div className="relative mx-auto flex h-full w-full max-w-[800px] flex-col items-start px-4 pt-6 sm:px-8 md:pt-16">
         <div className="relative grid aspect-square h-24 place-items-center overflow-hidden rounded-full bg-foreground/10">
           {user?.avatarImagePath ? (
             <Image unoptimized fill alt="" src={user?.avatarImagePath ?? ""} />
@@ -56,16 +59,32 @@ const Page = async ({ params }: PageProps) => {
         </h1>
         <p className="mt- text-foreground/75">0 Followers</p>
 
-        <ProfileActions id={user?.id.toString() ?? ""} />
+        <ProfileActions id={stringifyNumber(user?.id)} />
       </div>
 
-      <div className="mx-auto mt-14 flex w-full max-w-[1300px] items-center justify-between px-8">
+      <div className="mx-auto mt-14 flex w-full max-w-[1300px] items-center justify-between px-4 sm:px-8">
+        <UserProfileTabs />
+
+        <Button
+          className="hidden gap-2 sm:flex"
+          variant="outlined"
+          color="foreground"
+        >
+          Filter <Icon name="ArrowDown" className="text-[18px]" />
+        </Button>
+      </div>
+
+      <span className="mt-4 flex h-[1px] w-full bg-border" />
+
+      <div className="mx-auto my-4 flex w-full max-w-[1300px] items-center justify-between px-4 sm:hidden sm:px-8">
         <Button className="gap-2" variant="outlined" color="foreground">
           Filter <Icon name="ArrowDown" className="text-[18px]" />
         </Button>
       </div>
-      <span className="mt-4 flex h-[1px] w-full bg-border" />
-      <EmptySearchFetch />
+
+      <div className="mx-auto w-full max-w-[1300px] px-4 pb-8 sm:px-8 sm:py-8">
+        <UserStories id={stringifyNumber(user?.id)} />
+      </div>
     </main>
   );
 };
