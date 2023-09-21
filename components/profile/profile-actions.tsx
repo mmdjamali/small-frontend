93290348ba6fc605a2b8d16d206a673b80cb4b84
MmD <mmdj27634@gmail.com>
2023-09-21 12:10:54 -0700

    add : follow and unfollow functionality

--- components/profile/profile-actions.tsx
+++ components/profile/profile-actions.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useUser } from "@/hooks/use-user";
-import Icon from "./icon";
-import Button from "./ui/button";
+import Icon from "../icon";
+import Button from "../ui/button";
 import Link from "next/link";
 import { useMutation, useQuery, useQueryClient } from "react-query";
 import { useCustomFetch } from "@/hooks/use-custom-fetch";
